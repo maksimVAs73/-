5dd8314c350f04ef4dc161caa06f256a3d47bcea
maksimVAs73 <traffikingvraj@gmail.com>
2024-11-20 23:05:09 +0900

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительное целое число:");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int number = int.Parse(input); // Попытка преобразовать строку в число
+
+                if (number <= 0)
+                {
+                    throw new ArgumentException("Число должно быть положительным."); //Выбрасываем исключение, если число не положительное
+                }
+
+                Console.WriteLine($"Вы ввели число: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Неверный формат ввода.  Введите целое число.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла непредвиденная ошибка: {ex.Message}");
+            }
+        }
+    }
+}
